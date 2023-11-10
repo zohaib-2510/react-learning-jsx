a1c7952bafbde623cdf7022917b9d09e3c7f4596
zohaib-2510 <140811807+zohaib-2510@users.noreply.github.com>
2023-11-10 11:31:28 -0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Button from "./component/Button";
+function App() {
+  return (
+    <div>
+      <h1>hello world</h1>
+      <Button />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/Button/index.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import "./styles.css"
+function Button (){
+    return (
+        <button className="btn">click me</button>
+    )
+}
+
+export default Button
